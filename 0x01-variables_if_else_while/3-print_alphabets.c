457c01aa9d50c9eb0e1a222fb25f9631b01d85f1
farhan <fcstudy06@gmail.com>
2023-08-13 11:56:28 -0700

    alphapetics lowercase and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints alphapetics
+ * in  lowercase and uppercase
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
